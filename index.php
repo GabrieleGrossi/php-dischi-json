c6a8ebb65989c89c3b17f0112d2e63e0d38e0f56
GabrieleGrossi <grossigabriele98@gmail.com>
2023-07-05 14:48:19 +0200

    Import my style and now start work

--- index.php
+++ index.php
@@ -10,10 +10,19 @@
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <!-- Vue -->
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- My Style -->
+    <link rel="stylesheet" href="./style/style.css">
     <title>Php-Json Dischi</title>
 </head>
 <body>
+    <header>
+    <h1>
+        Sono l'header
+    </h1>
+    </header>
+
     <script src="./script/main.js"></script>
 </body>
 </html>
